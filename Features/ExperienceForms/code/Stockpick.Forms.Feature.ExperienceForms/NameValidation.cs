b58bd12b1c53c08a76ab06ca5acf83fa826e1af2
Jan Bluemink <jan@mirabeau.nl>
2018-02-01 17:22:25 +0100

    inital setup

---
+++ Features/ExperienceForms/code/Stockpick.Forms.Feature.ExperienceForms/NameValidation.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+
+using System.ComponentModel.DataAnnotations;
+
+using System.Text.RegularExpressions;
+
+using System.Web.Mvc;
+
+using Sitecore.ExperienceForms.Mvc.Models.Fields;
+
+using Sitecore.ExperienceForms.Mvc.Models.Validation;
+
+namespace Sitecore.ExperienceForms.Samples.Validation
+
+{
+
+    public class NameValidation : ValidationElement<string>
+
+    {
+
+        private const string NamePattern = "^[a-zA-Z ]*$";
+
+        public NameValidation(ValidationDataModel validationItem) : base(validationItem)
+
+        {
+
+        }
+
+        public override IEnumerable<ModelClientValidationRule> ClientValidationRules
+
+        {
+
+            get
+
+            {
+
+                var clientValidationRule = new ModelClientValidationRule
+
+                {
+
+                    ErrorMessage = FormatMessage(Title),
+
+                    ValidationType = "regex"
+
+                };
+
+                clientValidationRule.ValidationParameters.Add("pattern", NamePattern);
+
+                yield return clientValidationRule;
+
+            }
+
+        }
+
+        public string Title { get; set; }
+
+        public override ValidationResult Validate(object value)
+
+        {
+
+            if (value == null)
+
+            {
+
+                return ValidationResult.Success;
+
+            }
+
+            var regex = new Regex(NamePattern, RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture | RegexOptions.Compiled);
+
+            var stringValue = (string)value;
+
+            if (string.IsNullOrEmpty(stringValue) || regex.IsMatch(stringValue))
+
+            {
+
+                return ValidationResult.Success;
+
+            }
+
+            return new ValidationResult(FormatMessage(Title));
+
+        }
+
+        public override void Initialize(object validationModel)
+
+        {
+
+            base.Initialize(validationModel);
+
+            var obj = validationModel as StringInputViewModel;
+
+            if (obj != null)
+
+            {
+
+                Title = obj.Title;
+
+            }
+
+        }
+
+    }
+
+}
---
+++ Features/ExperienceForms/code/Stockpick.Forms.Feature.ExperienceForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Stockpick.Forms.Feature.ExperienceForms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mirabeau")]
+[assembly: AssemblyProduct("Stockpick.Forms.Feature.ExperienceForms")]
+[assembly: AssemblyCopyright("Copyright © Mirabeau 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0eb567c5-c58d-423c-9e2e-d86f140bc2c2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
